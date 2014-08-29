e6458a3b900f649d8883d6693f944907e9624cdc
Alex Birdsall & Hethe Berg <pair+ambirdsall.growthcode@devbootcamp.com>
2014-08-29 15:41:49 -0700

    Fixes models
    moves from individual folders to model folder
    fixes typo in polymorphic has_many associations

--- dbc_overflow/app/models/question.rb
+++ dbc_overflow/app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers
-  has_many :votes, as :vote_for
-  has_many :responses, as :response_to
+  has_many :votes, as: :vote_for
+  has_many :responses, as: :response_to
 end
