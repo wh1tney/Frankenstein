78fcdc837fa38402ee4b95cfcc3762134663c375
Tommy Dugger <tommy.dugger78@gmail.com>
2014-08-29 16:37:43 -0700

    Adds routes for loging in

--- dbc_overflow/config/routes.rb
+++ dbc_overflow/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
+  get '/session/login' to: 'session#login' as 'login'
+  post '/session' to: 'session#validate_credentials'
+
   get '/users/new', to: 'user#new', as: 'user_new'
   post '/users', to: 'user#create'
 
