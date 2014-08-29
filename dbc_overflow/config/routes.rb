c0d6b6ea61897cdb1e3f5825c3a1c640df636d50
Tommy Dugger <tommy.dugger78@gmail.com>
2014-08-29 16:42:32 -0700

    Fixes syntax errors in routes.rb for missing commas

--- dbc_overflow/config/routes.rb
+++ dbc_overflow/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  get '/session/login' to: 'session#login' as 'login'
-  post '/session' to: 'session#validate_credentials'
+  get '/session/login', to: 'session#login', as: 'login'
+  post '/session', to: 'session#validate_credentials'
 
   get '/users/new', to: 'user#new', as: 'user_new'
   post '/users', to: 'user#create'
