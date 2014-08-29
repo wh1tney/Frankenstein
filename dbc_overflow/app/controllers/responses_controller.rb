18e91b6e19ef7378250b6b2a83f7099ff3cf156c
Alex Birdsall & Hethe Berg <pair+ambirdsall.growthcode@devbootcamp.com>
2014-08-29 16:57:22 -0700

    Adds list-making logic for ResponsesController index method

--- dbc_overflow/app/controllers/responses_controller.rb
+++ dbc_overflow/app/controllers/responses_controller.rb
@@ -1,5 +1,14 @@
 class ResponsesController < ApplicationController
   def index
-    @responses = Response.all
+    response_to = find_response_to
+    @responses = response_to.responses
+  end
+  private
+  def find_response_to
+    case params[:response_to_type]
+    when "question"
+      Question.find[:response_to_id]
+    when "answer"
+      Answer.find[:response_to_id]
   end
 end
