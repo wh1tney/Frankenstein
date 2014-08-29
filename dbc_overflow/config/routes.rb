4faaf689d7890306d5be0bda2da5865674281ef5
Tommy Dugger <tommy.dugger78@gmail.com>
2014-08-29 16:07:19 -0700

    Fixes syntax error where commas where needed to seperate out the route from to and as

--- dbc_overflow/config/routes.rb
+++ dbc_overflow/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  get  '/users/new' to: 'user#new' as: 'user_new'
-  post '/users' to: 'user#create'
+  get '/users/new', to: 'user#new', as: 'user_new'
+  post '/users', to: 'user#create'
 
 end
