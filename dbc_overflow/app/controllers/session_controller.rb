03827364aac38c410a68fc752fa7900382c274d5
Tommy Dugger <tommy.dugger78@gmail.com>
2014-08-29 16:27:07 -0700

    Adds logic to login action of the session controller

--- dbc_overflow/app/controllers/session_controller.rb
+++ dbc_overflow/app/controllers/session_controller.rb
@@ -1,6 +1,10 @@
 class SessionController < ApplicationController
   def login
-
+    if session[:user_id].nil?
+      render :login
+    else
+      redirect_to root_path
+    end
   end
 
   def logout
