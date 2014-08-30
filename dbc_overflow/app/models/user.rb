00a9464a8146d3423e8f501f25e2eaed6a2b9603
Lauren Kroner <lauren.kroner@gmail.com>
2014-08-30 14:52:28 -0700

    Add faker gem dependency.

--- dbc_overflow/app/models/user.rb
+++ dbc_overflow/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  validates :email, :password, presence: true
-  validates :email, uniquesness: true
+  validates :email, presence: true
+  # validates :email, uniquesness: true
 
   has_many :questions
   has_many :answers
