af12390b706cd613e51a6ca28007c850ca73a126
Whitney O'Banner <wobanner@gmail.com>
2014-08-30 13:58:11 -0700

    Remove explicitly named route for users#new

--- dbc_overflow/config/routes.rb
+++ dbc_overflow/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/sessions/login', to: 'sessions#login', as: 'login'
   post '/sessions', to: 'sessions#validate_credentials'
 
-  get '/users/new', to: 'users#new', as: 'user_new'
+  get '/users/new', to: 'users#new'
   post '/users', to: 'users#create'
   
 end
