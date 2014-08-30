13675388059ba1bb31cbc7d5ac82335637444dd7
Whitney O'Banner <wobanner@gmail.com>
2014-08-30 15:15:17 -0700

    Modify answers controller actions
    * Implement answers#new action
    * Implement answers#create action
    * Set all actions to respond with html or js

--- dbc_overflow/app/controllers/answers_controller.rb
+++ dbc_overflow/app/controllers/answers_controller.rb
@@ -1,10 +1,20 @@
 class AnswersController < ActionController::Base
+  respond_to :html, :js
+
   def index
   end
 
   def new
+    answer = Answer.new
   end
 
   def create
+    answer = Answer.create(answer_params)
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:content)
   end
 end
