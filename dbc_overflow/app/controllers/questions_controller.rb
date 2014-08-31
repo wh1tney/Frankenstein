a83f710b519430d8073d53ca283a880631993263
Evan Simoni <evan.simoni@gmail.com>
2014-08-30 19:21:07 -0700

    adds attributes to the questions controller; makes them visible in the view

--- dbc_overflow/app/controllers/questions_controller.rb
+++ dbc_overflow/app/controllers/questions_controller.rb
@@ -1,19 +1,16 @@
 class QuestionsController < ApplicationController
   def index
-  end
 
+  end
   def new
     @question = Question.new
   end
-
   def create
     @question = Question.create
   end
-
   def show
     @question = Question.find(params[:id])
     @user = User.find(@question.user_id)
     render :index
   end
-
 end
