0360ad859eb3037485593cffb9281e7ee407c387
Whitney O'Banner <wobanner@gmail.com>
2014-08-31 20:20:26 -0700

    Add convenience method to Answer model
    * Method returns username associated with Answer
    * Display username of answerer in view

--- dbc_overflow/app/models/answer.rb
+++ dbc_overflow/app/models/answer.rb
@@ -6,4 +6,8 @@ class Answer < ActiveRecord::Base
   belongs_to :user
   has_many :responses, as: :response_to
   has_many :votes, as: :vote_for
+
+  def user
+    User.find(self.user_id.to_i).username
+  end
 end
